34215650cfd1efcc5f53fb994b08cdc82c8b03ad
Ethan Derouchie <46546803+ethanderouchie@users.noreply.github.com>
2021-10-27 17:14:39 +0000

    Allowed for the ability to select an animal.

--- AnimalCompetition/Main.java
+++ AnimalCompetition/Main.java
@@ -3,21 +3,26 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {   
     System.out.println("Choose Your Fighter!");
-    Scanner scanner = new Scanner (System.in); 
+    Scanner scanner = new Scanner (System.in);
     String fighter1 = ""; 
+    int animalCounter = 0;
 
     var listofanimals = new Animal[] {
       new Cat("Garfield"),
       new Dog("Fido"),
       new Raccoon("Panda"),
     };
+
+    
     
 
     var animals = new ArrayList<>(Arrays.asList(listofanimals));
+
+    int AL_AnimalsSize = animals.size();
+
+
+    String combatants[] = new String[AL_AnimalsSize];
     
-
-
-
     for (var animal: animals) { //adds the animals to the list of fighters
       System.out.print(String.format("I'm a %s, and my name is %s. ", animal.species(), animal.name()));
       var fighter = (IFightable)animal;
@@ -32,10 +37,38 @@ class Main {
       } else {
         System.out.println("I can't defend.");
       }
-  
+      combatants[animalCounter] = animal.species();
+      animalCounter++;
+
     }
 
+    for (int i = 0; i <= AL_AnimalsSize - 1; i++) {
+      System.out.println(combatants[i]);
+    }
+
+    fighter1 = getPlayerChoice(combatants, AL_AnimalsSize);
+
+
+    
+
   } //close main method
+
+  public static String getPlayerChoice(String combatants[], int AL_AnimalsSize) {
+    Scanner scanner = new Scanner (System.in);
+    String chosenFighter = "";
+    
+    boolean isAFighter = false;
+    while (isAFighter != true) {
+      chosenFighter = scanner.nextLine();
+      chosenFighter = chosenFighter.toLowerCase();
+      for (int i = 0; i <= AL_AnimalsSize - 1; i++) {
+        if (chosenFighter.equals(combatants[i])) {
+          isAFighter = true;
+        }
+      }
+    }
+    return chosenFighter;
+  }
 
   
 
